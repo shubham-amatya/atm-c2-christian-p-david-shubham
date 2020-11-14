1391f00228d2288b9b195d23a619bf650f9d102b
DavidToribio1 <72708052+DavidToribio1@users.noreply.github.com>
2020-11-14 18:17:42 -0500

    Revert "updated user"
    This reverts commit 41b247748d1dc471d93fe8cdd89b483ba4b9734b.

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,55 +2,41 @@ import java.util.ArrayList;
 
 public class User {
 
-    private String ID;
+    private String name;
     private String password;
     ArrayList <Account> Accounts = new ArrayList<Account>();
     private boolean loggedin;
 
 
-    public User(String ID, String password) {
-        this.ID = ID;
+    public User(String name, String password) {
+        this.name = name;
         this.password = password;
         this.Accounts = new ArrayList<Account>();
 
 
     }
-    public String getID(){
-     return ID;
+    public String getName(){
+     return name;
 
     }
-    public ArrayList<Account> getAccount(){
-       return Accounts;
+    public String getAccount(){
+       return Account;
 
     }
-    public boolean authenticate(String password){
-        if (password.equals(this.password)){
-            return true;
-        }
-        return false;
-   }
-
-    public void   createAccount(Integer Account){
-        switch (Account)
-        {
-            case 1:
-
-                Accounts.add(new CheckingAccount(0.0));
-                break;
-            case 2:
-
-                Accounts.add(new SavingsAccount(0.0));
-                break;
-            case 3:
-
-                Accounts.add(new InvestmentAccount(0.0));
-                break;
-        }
+    public String authenticate(String name, String password){
 
     }
 
+    public String createAccount(){
 
-    public boolean getloggedin(){
-    return loggedin;
+        return Account;
+    }
+
+    public String closeAccount(){
+        return Account;
+    }
+
+    public getloggedin(){
+
     }
 }
