ad5ae6e1a98878ff4c298f60d3f50a4f4685c9e8
shubham <shubham@zipcodes-MBP-5.fios-router.home>
2020-11-14 14:21:46 -0500

    Console demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
+import java.io.IOException;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args)  {
+        Console myConsole = new Console();
+        myConsole.welcomeInput();
+        myConsole.accountTypeMenu();
+        myConsole.optionsMenu();
         
     }
 }
